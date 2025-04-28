03ff4d2aaab0373e12af58d0728a9427f7881cb2
tynoerm <tinomutendaishemutemaringa@gmail.com>
2025-04-28 15:28:54 +0100

    Add files via upload

---
+++ backend/models/TruckserviceModule/Truckservice.js
@@ -0,0 +1,29 @@
+import { Schema as _Schema, model } from 'mongoose';
+const Schema = _Schema;
+
+let truckserviceSchema = new Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  driverName: {
+    type: String,
+    required: true
+  },
+  truckName: {
+    type: String,
+    required: true
+  },
+  serviceMileage: {
+    type: Number,
+    required: true
+  },
+  nextServiceMileage: {
+    type: Number,
+    required: true
+  }
+}, {
+  collection: 'truckservice'
+});
+
+export default model('TruckService', truckserviceSchema);
