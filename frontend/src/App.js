1d5effefc6c307cbd6454144cc47b1b3f06e8d13
tynoerm <tinomutendaishemutemaringa@gmail.com>
2025-04-28 14:29:31 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import Dashboard from "./Dashboard.js"
 
-import SalesModuleDashboard from './SalesModule/Dashboard.js';
-import Sales from "./SalesModule/Sales.js"
+
+
 
 import Expenses from './ExpensesModule/Expenses.js';
 import Invoices from './InvoicesModule/Invoices.js';
-import Quotation from './SalesModule/Quotation.js';
-import SalesReports from './SalesModule/SalesReports.js';
+import Truckservice from './TruckserviceModule/Truckservice.js';
+
+
 import Orders from './OrdersModule/Orders.js'
 import Payments from './PaymentsModule/Payments.js';
 
@@ -25,12 +26,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="" element={<Dashboard/>} />
-        <Route path="Sales" element={<Sales/>} />
-        <Route path="SalesModuleDashboard" element={<SalesModuleDashboard/>} />    
-        <Route path="Quotation" element={<Quotation/>} />
+    
+      
         <Route path="Expenses" element={<Expenses/>} />
         <Route path="Invoices" element={<Invoices/>} />
-        <Route path="SalesReports" element={<SalesReports/>} />
+        <Route path="Truckservice" element={<Truckservice/>} />
         <Route path="Orders" element={<Orders/>} />
         <Route path="Payments" element={<Payments/>} />
 
