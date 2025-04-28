9252f1615df50a54efff60d27d6cc8bc13349ebe
tynoerm <tinomutendaishemutemaringa@gmail.com>
2025-04-28 15:29:39 +0100

    Add files via upload

---
+++ backend/routes/TruckserviceModule/Truckservice.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import express from "express";
+import PDFDocument from "pdfkit";
+import ExcelJS from "exceljs";
+
+import truckserviceSchema from "../../models/TruckserviceModule/Truckservice.js";  // Updated import
+
+let router = express.Router();
+
+
+router.route("/create-truckservice").post(async (req, res, next) => {
+  await truckserviceSchema
+      .create(req.body)
+      .then((result) => {
+          res.json({
+              data: result,
+              message: "record created successfully",
+              status: 200,
+          });
+      })
+      .catch((err) => {
+          console.log(err); // FIX: Change `console.log(data.err)` to `console.log(err)`
+          return next(err);
+      });
+});
+router.route("/").get(async (req, res, next) => {  // Updated route
+  await truckserviceSchema
+    .find()
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Orders fetched successfully",  // Updated message
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+
+
+export { router as truckserviceRoutes };  // Updated export name
